202902f9aab8fa7965decf511d7dc26d4b8dcd04
tokomine <tokomine.s@gmail.com>
2023-09-13 16:28:54 +0800

    init

---
+++ Codeforces/1868_Codeforces Round 896 (Div. 1)/D_Flower-like Pseudotree/D_Flower-like Pseudotree.cpp
@@ -0,0 +1,43 @@
+// C++ lib
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <complex>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <array>
+#include <deque>
+#include <list>
+#include <valarray>
+#include <regex>
+#include <forward_list>
+#include <initializer_list>
+#include <tuple>
+
+// C lib
+#include <cstdint>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <cctype>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    return 0;
+}
