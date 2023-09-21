142d14660a64ed1fb2071cd039eca0625002a115
tokomine <tokomine.s@gmail.com>
2023-09-21 22:23:12 +0800

    update

---
+++ LeetCode/2769.find-the-maximum-achievable-number.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=2769 lang=cpp
+ *
+ * [2769] Find the Maximum Achievable Number
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    int theMaximumAchievableX(int num, int t) { return num + t * 2; }
+};
+// @lc code=end
